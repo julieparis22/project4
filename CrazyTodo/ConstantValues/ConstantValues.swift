09c0a3efd16dfa436405ac2b8ed536a3a6083531
julie <julie.ryan.france@gmail.com>
2024-07-20 19:19:50 +0200

    passage de changement de couleur d une view a l autre fonctionne

--- CrazyTodo/ConstantValues/ConstantValues.swift
+++
@@ -1,9 +0,0 @@
-//
-//  ConstantValues.swift
-//  CrazyTodo
-//
-//  Created by julie ryan on 19/07/2024.
-//
-
-import Foundation
-public let Categories = ["other", "work", "leisure","family","friends" ]
---
+++ CrazyTodo/ModelView/InputContentView.swift
@@ -0,0 +1,18 @@
+//
+//  InputContentView.swift
+//  CrazyTodo
+//
+//  Created by julie ryan on 20/07/2024.
+//
+
+import SwiftUI
+
+struct InputContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InputContentView()
+}
